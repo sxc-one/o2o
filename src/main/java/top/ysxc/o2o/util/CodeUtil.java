c04b5b4a48b0d56dcb9eaf1b7ece6c13bfe54b30
sxc-one <1693228778@qq.com>
2021-01-29 16:41:29 +0800

    4-12引入kaptcha实现验证码

---
+++ src/main/java/top/ysxc/o2o/util/CodeUtil.java
@@ -0,0 +1,23 @@
+package top.ysxc.o2o.util;
+
+import com.google.code.kaptcha.Constants;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author ysxc
+ * @create 2021-01-29 4:25 下午
+ */
+public class CodeUtil {
+
+    public static boolean checkVerifyCode(HttpServletRequest request) {
+        String verifyCodeExpected = (String)request.getSession().getAttribute(
+                Constants.KAPTCHA_SESSION_KEY
+        );
+        String verifyCodeActual = HttpServletRequestUtil.getString(request, "verifyCodeActual");
+        if (verifyCodeActual == null || !verifyCodeActual.equals(verifyCodeExpected)) {
+            return false;
+        }
+        return true;
+    }
+}
--- src/main/java/top/ysxc/o2o/web/shopadmin/ShopManagementController.java
+++ src/main/java/top/ysxc/o2o/web/shopadmin/ShopManagementController.java
@@ -21,6 +21,7 @@ import top.ysxc.o2o.exceptions.ShopOperationException;
 import top.ysxc.o2o.service.AreaService;
 import top.ysxc.o2o.service.ShopCategoryService;
 import top.ysxc.o2o.service.ShopService;
+import top.ysxc.o2o.util.CodeUtil;
 import top.ysxc.o2o.util.HttpServletRequestUtil;
 
 import javax.servlet.http.HttpServletRequest;
@@ -70,8 +71,12 @@ public class ShopManagementController {
     @ResponseBody
     private Map<String, Object> registerShop(HttpServletRequest request) {
         Map<String, Object> modelMap = new HashMap<>();
-        //TODO:验证码逻辑
-
+        //验证码
+        if (!CodeUtil.checkVerifyCode(request)) {
+            modelMap.put("success", false);
+            modelMap.put("errMsg", "输入了错误的验证码");
+            return modelMap;
+        }
         // 1.接收并转化相应的参数，包括店铺信息以及图片信息
         String shopStr = HttpServletRequestUtil.getString(request, "shopStr");
         ObjectMapper mapper = new ObjectMapper();
