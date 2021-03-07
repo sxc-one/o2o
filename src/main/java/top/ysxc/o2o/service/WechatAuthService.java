67a3dbb498849801476aff231ec5a8e574cf509e
sxc-one <1693228778@qq.com>
2021-03-07 17:44:58 +0800

    10-1到10-9 阿里云部署&微信测试号&远程开发调试&端口转发

---
+++ src/main/java/top/ysxc/o2o/service/WechatAuthService.java
@@ -0,0 +1,27 @@
+package top.ysxc.o2o.service;
+
+import top.ysxc.o2o.dto.WechatAuthExecution;
+import top.ysxc.o2o.entity.WechatAuth;
+import top.ysxc.o2o.exceptions.WechatAuthOperationException;
+
+public interface WechatAuthService {
+
+	/**
+	 * 通过openId查找平台对应的微信帐号
+	 * 
+	 * @param openId
+	 * @return
+	 */
+	WechatAuth getWechatAuthByOpenId(String openId);
+
+	/**
+	 * 注册本平台的微信帐号
+	 * 
+	 * @param wechatAuth
+	 * @param profileImg
+	 * @return
+	 * @throws RuntimeException
+	 */
+	WechatAuthExecution register(WechatAuth wechatAuth) throws WechatAuthOperationException;
+
+}
--- src/main/java/top/ysxc/o2o/util/PathUtil.java
+++ src/main/java/top/ysxc/o2o/util/PathUtil.java
@@ -7,7 +7,6 @@ package top.ysxc.o2o.util;
 public class PathUtil {
 
     private static final String seperator = System.getProperty("file.separator");
-
     public static String getImgBasePath() {
         String os = System.getProperty("os.name");
         String basePath = "";
